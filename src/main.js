ffb307f4b7a1898b0f758a06691cd52f5d242f44
noontage <noontage@example.com>
2018-09-06 00:47:10 +0900

    wrapped: mrb_irep

--- src/main.js
+++ src/main.js
@@ -1,19 +1,48 @@
 import Module from './mqrb-core';
 import Mqrb from './mqrb-wrap';
+window.onload = () => 
+{
 
-fetch('mqrb-core.wasm')
-  .then(response => response.arrayBuffer())
-  .then(buffer => new Uint8Array(buffer))
-  .then(binary => {
-    var moduleArgs = {
-      wasmBinary: binary,
-      onRuntimeInitialized: function () {
-        const mqrb = Mqrb(module);
-        mqrb.initialize();
-        var mrb = mqrb.create_instance();
-        mqrb.exec_script(mrb, "p (1..10).class")
-        mqrb.delete_instance(mrb);
-      }
-    };
-    var module = Module(moduleArgs);
-  });
+  var status = {
+    loaded: false
+  }
+  var el_progress = document.getElementById("progress");
+  var el_contents = document.getElementById("contents")
+
+  setTimeout(() => {
+    if (!status.loaded) {
+      el_progress.style.display = "block";
+    }
+  }, 1500);
+
+  function setStatusVmLoaded() {
+    status.loaded = true;
+    el_progress.style.display = "none";
+    el_contents.style.display = "block";
+  }
+
+  fetch('mqrb-core.wasm')
+    .then(response => response.arrayBuffer())
+    .then(buffer => new Uint8Array(buffer))
+    .then(binary => {
+      var moduleArgs = {
+        wasmBinary: binary,
+        onRuntimeInitialized: () => {
+          setStatusVmLoaded();
+
+          // Ruby VM Instance
+          const mqrb = Mqrb(module);
+          var mrb = mqrb.create_instance();
+          mqrb.exec_script(mrb, "a = 0 ;p (1..10).class; p a")
+          mqrb.delete_instance(mrb);
+
+          var mrb = mqrb.create_instance();
+          console.log(mrb);
+          mqrb.exec_script(mrb, "p a")
+          mqrb.delete_instance(mrb);
+        }
+      };
+      var module = Module(moduleArgs);
+    });
+
+}
--- src/mqrb-wrap.js
+++ src/mqrb-wrap.js
@@ -1,8 +1,11 @@
 export default function (module) {
+  var mqrb_initialize = module.cwrap('mqrb_initialize', 'number');
+  mqrb_initialize();
+
   return ({
-    initialize: module.cwrap('mqrb_initialize', 'number'),
     create_instance: module.cwrap('mqrb_create_instance', 'number'),
     delete_instance: module.cwrap('mqrb_delete_instance', 'number', ['number']),
-    exec_script: module.cwrap('mqrb_exec_script', null, ['number', 'string'])
+    exec_script: module.cwrap('mqrb_exec_script', null, ['number', 'string']),
+    exec_irep: module.cwrap('mqrb_exec_irep', null, ['number', 'array'])
   });
 }
